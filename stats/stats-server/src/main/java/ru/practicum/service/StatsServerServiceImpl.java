e0a1416456a1372cf4357abd2548ddf4985fe8d8
Shtainbook <shtainbook@yandex.ru>
2023-07-02 15:24:34 +0300

    +@repository +@service -import

--- stats/stats-server/src/main/java/ru/practicum/service/StatsServerServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatsServerServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.practicum.mapper.EndPointHitMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.repository.StatsServerRepository;
